5080affddf03786a60c8d069801b56852a7b6a96
VinVaz <viniciusvazpro@gmail.com>
2020-09-29 16:50:48 -0300

    added the avarage male age

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -8,5 +8,18 @@ adult_file = pd.read_csv("./adult.data", names=["age","workclass","fnlwgt",
                                                 "hours-per-week",
                                                 "native-country","salary"])
                                                 
-print(adult_file)
 
+
+
+  
+#def calculate_demographic_data (data):
+  
+# How many people of each race are represented in this dataset?  
+race_series = adult_file["race"].value_counts()
+
+# What is the average age of men?
+age_series = adult_file["age"]
+sex_series = adult_file['sex'].fillna('')
+
+male_age = age_series[sex_series.str.strip()=='Male']
+avg_male_age = male_age.mean()
